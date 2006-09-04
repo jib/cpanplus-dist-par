9c42bb346997254c73e516904aa2e644e86e8100
Jos Boumans <kane@cpan.org>
2006-09-04 18:27:19 +0000

    r779@chaos:  kane | 2006-09-04 20:20:19 +0200  * first time add of cpanplus-dist-par  * XXX this has a bug in PAR::Dist where dist name are    not being created properly without a good META.yml
    git-svn-id: http://oss.dwim.org/cpanplus-dist-par@770 4dccba1d-3c1b-0410-aec4-feb514a8dabc

--- lib/CPANPLUS/Dist/PAR.pm
+++ lib/CPANPLUS/Dist/PAR.pm
@@ -8,23 +8,47 @@ $VERSION =  '0.01';
 use CPANPLUS::Error;
 use CPANPLUS::Internals::Constants;
 
+use File::Basename              qw[basename];
 use Params::Check               qw[check];
-use Module::Load::Conditional   qw[can_load check_install];
+use Module::Load::Conditional   qw[can_load];
 use Locale::Maketext::Simple    Class => 'CPANPLUS', Style => 'gettext';
 
 local $Params::Check::VERBOSE = 1;
 
+=head1 NAME
+
+CPANPLUS::Dist::PAR
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=cut
+
+### we can't install things withour our dependencies.
 sub format_available { 
-    return unless can_load( 'PAR'       => 0,
-                            'PAR::Dist' => 0 );
+    return unless can_load( modules => {
+                            'PAR'       => 0,
+                            'PAR::Dist' => 0 
+                        } );
     return 1;
 }
 
-sub init { 
-    return 1;
-}
+
+### we dont need anything special
+sub init { return 1; }
 
 sub prepare { 
+    ### just in case you already did a create call for this module object
+    ### just via a different dist object
+    my $dist        = shift;
+    my $self        = $dist->parent;
+    my $dist_cpan   = $self->status->dist_cpan;
+
+    $dist->status->prepared( $dist_cpan->prepare( @_ ) );
+}
+
+sub create { 
     ### just in case you already did a create call for this module object
     ### just via a different dist object
     my $dist        = shift;
@@ -37,24 +61,76 @@ sub prepare {
     my $conf = $cb->configure_object;
     my %hash = @_;
 
-    my $fail;
-    
-    ### build it first
-    DIST_CPAN: {
-        $dist_cpan->prepare( @_ ) or ++$fail, last DIST_CPAN;
-        $dist_cpan->create(  @_ ) or ++$fail, last DIST_CPAN;
+    my $verbose;
+    {   local $Params::Check::ALLOW_UNKNOWN = 1;
+        my $tmpl = {
+            verbose => { default => $conf->get_conf('verbose'), 
+                         store => \$verbose
+                    },
+        };
+        check( $tmpl, \%hash ) or return;
     }
 
-    return $dist->status->prepared(0) if $fail;
+    ### first build it using the dist_cpan object
+    $dist->status->created( $dist_cpan->create( %hash ) ) or return;
     
-    
+    msg( loc("Creating PAR dist of '%1'", $self->name), $verbose);
 
+    ### par::dist is noisy, silence it
+    ### XXX this doesn't quite work -- restoring STDOUT still has
+    ### it closed
+    #*STDOUT_SAVE = *STDOUT; close *STDOUT;
+    my $par = eval {
+        PAR::Dist::blib_to_par( path => $self->status->extract );
+    };          
+    
+    ### error?
+    if( $@ or not $par or not -e $par ) {
+        error(loc("Could not create PAR distribution of %1: %2",
+                  $self->name, $@ ));
+        return;                  
+    }
+
+    my ($to,$fail);
+    MOVE: {
+        my $dir = File::Spec->catdir(
+                        $conf->get_conf('base'),
+                        CPANPLUS::Internals::Utils
+                           ->_perl_version(perl => $^X),
+                        $conf->_get_build('distdir'),
+                        'PAR'
+                    );      
+        my $to  = File::Spec->catfile( $dir, basename($par) );
+
+        $cb->_mkdir( dir => $dir )              or $fail++, last MOVE;                     
+        $cb->_move( file => $par, to => $to )   or $fail++, last MOVE;                     
+        msg(loc("PAR distribution written to: '%1'", $to), $verbose);
+    
+        $dist->status->dist( $to );
+    } 
+    
+    return 1 unless $fail;
+    return;
 }
 
-sub create { }
+sub install { 
+    ### just in case you already did a create call for this module object
+    ### just via a different dist object
+    my $dist        = shift;
+    my $self        = $dist->parent;
+    my $dist_cpan   = $self->status->dist_cpan;    
 
-sub install { }
+    $dist->status->install( $dist_cpan->install( @_ ) );
+}
 
-sub uninstall { }
+sub uninstall { 
+    ### just in case you already did a create call for this module object
+    ### just via a different dist object
+    my $dist        = shift;
+    my $self        = $dist->parent;
+    my $dist_cpan   = $self->status->dist_cpan;    
+
+    $dist->status->uninstall( $dist_cpan->uninstall( @_ ) );
+}
 
 1;
---
+++ t/src/Build/noxs/Foo-Bar/blib/script/foobar.pl
@@ -0,0 +1,8 @@
+#!/opt/bin/perl 
+
+eval 'exec /opt/bin/perl  -S $0 ${1+"$@"}'
+    if 0; # not running under some shell
+
+print "foobar";
+
+1;
